ea6d12c32b0a11f6f3c74e27ab0f4b8c4f451e42
Kamil Abzalov <mail@kamil-abzalov.com>
2022-08-05 23:24:14 +0300

    Working on design system; removed bootstrap classes

--- src/app/shared/header/navigation/navigation.component.ts
+++ src/app/shared/header/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'appla-navigation',
@@ -6,11 +6,5 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class NavigationComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class NavigationComponent {
 }
