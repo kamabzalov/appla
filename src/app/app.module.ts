e71d5eec0bc476527a1d7f2b50fe62a6e0373f62
Kamil Abzalov <mail@kamil-abzalov.com>
2022-08-09 22:54:05 +0300

    Added tiles with new images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TileComponent } from './home/tile/tile.component';
-import { ProductCategoryTileComponent } from './home/product-category-tile/product-category-tile.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    TileComponent,
-    ProductCategoryTileComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     HttpClientModule,
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
---
+++ src/app/home/home.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { ProductCategoryTileComponent } from './product-category-tile/product-category-tile.component';
+import { TileComponent } from './tile/tile.component';
+import { HomeComponent } from './home.component';
+import { SharedModule } from '../shared/shared.module';
+import { StoreOffersComponent } from './store-offers/store-offers.component';
+
+@NgModule({
+  declarations: [ProductCategoryTileComponent, TileComponent, HomeComponent, StoreOffersComponent],
+  imports: [CommonModule, HomeRoutingModule, SharedModule],
+})
+export class HomeModule {}
