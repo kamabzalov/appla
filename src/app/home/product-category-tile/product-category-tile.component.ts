a1ab3c9655660e2af0bc91d646a1591db24d31c6
Kamil Abzalov <mail@kamil-abzalov.com>
2022-08-14 00:19:55 +0300

    Added API for category page and product page

--- src/app/home/product-category-tile/product-category-tile.component.ts
+++ src/app/home/product-category-tile/product-category-tile.component.ts
@@ -25,5 +25,5 @@ export class ProductCategoryTileComponent {
   @Input() public link: string;
   @Input() public title: string;
   @Input() public categoryId: string;
-  @Input() entities: ProductInTile[];
+  @Input() public entities: ProductInTile[];
 }
