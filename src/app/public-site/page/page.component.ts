5ef759d2b224a90581a6e03587581061fed4d887
Kamil Abzalov <mail@kamil-abzalov.com>
2022-08-22 14:25:49 +0300

    App modules refactoring

--- src/app/public-site/page/page.component.ts
+++ src/app/public-site/page/page.component.ts
@@ -1,16 +1,9 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'appla-page',
   templateUrl: './page.component.html',
   styleUrls: ['./page.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class PageComponent {}
