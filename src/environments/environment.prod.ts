b498c85677d49a844db84da09b6e5e418fa6c582
Kamil Abzalov <mail@kamil-abzalov.com>
2022-09-25 21:45:57 +0300

    Added firebase config for production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
   production: true,
+  firebase: {
+    apiKey: 'AIzaSyAdncnxWX03eIxNlMzGGiQi9pTtnCQNDO4',
+    authDomain: 'appla-login.firebaseapp.com',
+    projectId: 'appla-login',
+    storageBucket: 'appla-login.appspot.com',
+    messagingSenderId: '631276837722',
+    appId: '1:631276837722:web:b9c880ecf3402453adf163',
+    measurementId: 'G-E7ZSDCTH4D',
+  },
 };
